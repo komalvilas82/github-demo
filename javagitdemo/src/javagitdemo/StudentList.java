d2717ef3433b125c46d7ad9ff6521060d4cc4584
komalvilas82 <komalvilas82@gmail.com>
2022-07-23 16:28:29 -0400

    adding student list

---
+++ javagitdemo/src/javagitdemo/StudentList.java
@@ -0,0 +1,18 @@
+package javagitdemo;
+
+import java.util.ArrayList;
+
+public class StudentList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Komal");
+		al.add("Kinjal");
+	
+	for(String l : al) {
+		System.out.println(l);
+	}
+	
+	}
+}
